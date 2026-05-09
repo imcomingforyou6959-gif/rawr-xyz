c1fe8c44e0abe20f63224373702c2f559ceee243
onlyonestands <imcomingforyou6959@gmail.com>
2026-05-09 14:00:18 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
-
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        background: '#050505',
+        primary: '#ef4444',
+        foreground: '#ffffff',
+      }
+    }
+  }
+};
